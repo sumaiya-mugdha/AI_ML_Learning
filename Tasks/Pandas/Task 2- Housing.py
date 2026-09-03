9ed2db5870bf3446f67585e33c666b710332ab2d
sumaiya-mugdha <mugdhakhan720@gmail.com>
2026-09-03 13:24:31 +0600

    practiced some more in campus

--- Tasks/Pandas/Task 2- Housing.py
+++ Tasks/Pandas/Task 2- Housing.py
@@ -1,7 +1,23 @@
 import pandas as pd
 df= pd.read_csv("../../housetraning.csv")
-#missing value zero chara ber korbe
+
+print(df.head())
+print(df.tail())
+print(df.info())
 missing = df.isna().sum()
 print(missing[missing > 0])
-#print(df.to_string())
+print(df.describe())
+print(df.duplicated().sum())
+print(df[["Alley","FireplaceQu","GarageType"]])
+print(df.iloc[4:10, 5:10 ])
+
+
+
+
+
+
+# #missing value zero chara ber korbe
+# missing = df.isna().sum()
+# print(missing[missing > 0])
+# #print(df.to_string())
 
